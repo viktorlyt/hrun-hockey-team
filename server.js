d5955cf7656c7d57ac86010016d716c4338720dc
anastasia-dorfman <anastasia.dorfman1@gmail.com>
2024-06-27 14:21:55 -0300

    server structure added

---
+++ server.js
@@ -0,0 +1,47 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+//routers
+//import  from "./routes";
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+
+//app.use("url", someRouter);
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
+
+const port = process.env.PORT || 5100;
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
