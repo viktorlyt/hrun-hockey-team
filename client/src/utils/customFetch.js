d1b2ff31be3a1eddbef30eeb4335df88837c431a
Viktorlyt <viktorlyt@gmail.com>
2024-09-20 13:50:08 -0600

    custom fetch changed

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+
+// Use the environment variable for the base URL
+const baseURL = import.meta.env.VITE_API_URL || "/api"; // Fallback to /api for local development
+
 const customFetch = axios.create({
-  baseURL: "/api",
+  baseURL,
   withCredentials: true,
 });
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Use environment variables from process.env inside Vite config
-const isProduction = process.env.VITE_NODE_ENV === "production";
-
-// Set the target URL based on the environment
-const targetUrl = isProduction
-  ? "https://hrun-hockey-team.onrender.com/api" // Remote backend for production
-  : "http://localhost:5101/api"; // Local backend for development
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: targetUrl,
+        target: "http://localhost:5101/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
